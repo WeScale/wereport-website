eaabd0b7bc797cf5fd94f91418b0ad195911aa07
Sebastien Lavayssiere <sebastien.lavayssiere@wescale.fr>
2017-03-30 17:05:03 +0200

    add tag in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 npm run build 
+version=$(npm version patch)
 
 cd build/
+
+sed -i.bak s/VERSION_TAG/$version/g index.html
 
 mv config.json config.dev.json
 mv config.prd.json config.json
@@ -16,3 +19,7 @@ mv config.dev.json config.json
 cd ..
 
 curl http://wereport-dev.gcp.wescale.fr/config.json -v -H 'Cache-Control: no-cache'
+
+git tag -a $version -m "new version $version"
+git push
+
